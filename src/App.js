dd519023b0ddad1e498e70cef3203ac54bc91cad
GiaKhai <65885232+GiaKhai@users.noreply.github.com>
2021-10-30 09:18:06 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <div>Nguyen Dinh Gia Khai</div>
+        <div>Hom nay demo</div>
       </header>
     </div>
   );
